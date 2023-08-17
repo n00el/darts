b6f875b420e55db315d75a20b092b7a10eb348a5
Noel Tompos <noel.tompos@shareline.hu>
2023-08-17 10:04:45 +0200

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import styles from './page.module.css';
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h1 className={styles.title}>Placeholder míg Martin meg nem csinálja</h1>
+			<h1
+				className={styles.title}
+				style={{
+					display: 'flex',
+					justifyContent: 'center'
+				}}
+			>
+				Placeholder míg Martin meg nem csinálja
+			</h1>
 		</main>
 	);
 }
